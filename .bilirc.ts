fed2ab5727625ad2283790edd0f0221dbf8ec11d
MunifTanjim <muniftanjim@gmail.com>
2019-12-01 00:14:10 +0600

    Update project setup

--- .bilirc.ts
+++ .bilirc.ts
@@ -1,11 +1,36 @@
 type Config = import('bili').Config
 
 const config: Config = {
-  input: 'src/index.ts',
+  input: ['src/index.ts'],
   output: {
     dir: 'lib',
     format: ['cjs', 'esm']
+  },
+  plugins: {
+    typescript2: {
+      tsconfigOverride: {
+        compilerOptions: {}
+      }
+    }
   }
+}
+
+const { SYNOR_DATABASE, SYNOR_SOURCE } = process.env
+
+if (SYNOR_DATABASE) {
+  config.input = `src/database/${SYNOR_DATABASE}/index.ts`
+  config.output.dir = 'lib/database'
+  config.output.format = 'cjs'
+  config.output.fileName = `${SYNOR_DATABASE}[ext]`
+  config.plugins.typescript2.tsconfigOverride.compilerOptions.declaration = false
+}
+
+if (SYNOR_SOURCE) {
+  config.input = `src/source/${SYNOR_SOURCE}/index.ts`
+  config.output.dir = 'lib/source'
+  config.output.format = 'cjs'
+  config.output.fileName = `${SYNOR_SOURCE}[ext]`
+  config.plugins.typescript2.tsconfigOverride.compilerOptions.declaration = false
 }
 
 export default config
--- .eslintrc.js
+++ .eslintrc.js
@@ -10,10 +10,13 @@ module.exports = {
     createDefaultProgram: true
   },
   rules: {
+    '@typescript-eslint/camelcase': 'warn',
     '@typescript-eslint/consistent-type-definitions': 'off',
     '@typescript-eslint/no-empty-interface': 'warn',
     '@typescript-eslint/no-non-null-assertion': 'warn',
     '@typescript-eslint/promise-function-async': 'off',
+    '@typescript-eslint/require-array-sort-compare': 'off',
+    '@typescript-eslint/require-await': 'warn',
     '@typescript-eslint/strict-boolean-expressions': 'off'
   }
 }
