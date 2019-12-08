61a38b3adfd2de90178c8f6a14d377a81f5b7a5d
MunifTanjim <muniftanjim@gmail.com>
2019-12-08 19:17:15 +0600

    Refactor core/event-emitter

--- src/core/event-emitter.ts
+++ src/core/event-emitter.ts
@@ -4,63 +4,45 @@ type MigrationSource = import('./migration').MigrationSource
 type MigrationVersion = import('./migration').MigrationVersion
 type MigrationHistory = import('./migration').MigrationHistory
 
-type SynorMigratorEvent =
-  | 'lock:start'
-  | 'lock:end'
-  | 'unlock:start'
-  | 'unlock:end'
-  | 'open:start'
-  | 'open:end'
-  | 'close:start'
-  | 'close:end'
-  | 'drop:start'
-  | 'drop:end'
-  | 'version:start'
-  | 'version'
-  | 'version:end'
-  | 'history:start'
-  | 'history'
-  | 'history:end'
-  | 'pending:start'
-  | 'pending'
-  | 'pending:end'
-  | 'validate:start'
-  | 'validate:end'
-  | 'migrate:start'
-  | 'migrate:run:start'
-  | 'migrate:run:end'
-  | 'migrate:end'
-  | 'repair:start'
-  | 'repair:end'
+type SynorMigratorEvent = {
+  'lock:start': []
+  'lock:end': []
+  'unlock:start': []
+  'unlock:end': []
+  'open:start': []
+  'open:end': []
+  'close:start': []
+  'close:end': []
+  'drop:start': []
+  'drop:end': []
+  'version:start': []
+  version: [MigrationVersion]
+  'version:end': []
+  'history:start': []
+  history: [MigrationHistory]
+  'history:end': []
+  'pending:start': []
+  pending: [MigrationSource[]]
+  'pending:end': []
+  'validate:start': []
+  'validate:end': []
+  'migrate:start': []
+  'migrate:run:start': [MigrationSource]
+  'migrate:run:end': [MigrationSource]
+  'migrate:end': []
+  'repair:start': []
+  'repair:end': []
+}
 
-type EventWithData =
-  | 'version'
-  | 'history'
-  | 'pending'
-  | 'migrate:run:start'
-  | 'migrate:run:end'
-
-type EmitEvent<Event extends SynorMigratorEvent, Data = void> = (
-  event: Event,
-  data: Data
+type SynorEventEmitterEmit = <EventName extends keyof SynorMigratorEvent>(
+  event: EventName,
+  ...data: SynorMigratorEvent[EventName]
 ) => void
 
-type SynorEventEmitterEmit = EmitEvent<'version', MigrationVersion> &
-  EmitEvent<'history', MigrationHistory> &
-  EmitEvent<'pending', MigrationSource[]> &
-  EmitEvent<'migrate:run:start' | 'migrate:run:end', MigrationSource> &
-  EmitEvent<Exclude<SynorMigratorEvent, EventWithData>>
-
-type OnEvent<Event extends SynorMigratorEvent, Data = void> = (
-  event: Event,
-  listener: (data: Data) => void
+type SynorEventEmitterOn = <EventName extends keyof SynorMigratorEvent>(
+  event: EventName,
+  listener: (...data: SynorMigratorEvent[EventName]) => void
 ) => void
-
-type SynorEventEmitterOn = OnEvent<'version', MigrationVersion> &
-  OnEvent<'history', MigrationHistory> &
-  OnEvent<'pending', MigrationSource[]> &
-  OnEvent<'migrate:run:start' | 'migrate:run:end', MigrationSource> &
-  OnEvent<Exclude<SynorMigratorEvent, EventWithData>>
 
 type SynorEventEmitter = {
   emit: SynorEventEmitterEmit
@@ -70,11 +52,12 @@ type SynorEventEmitter = {
 export function SynorEventEmitter(): SynorEventEmitter {
   const eventEmitter = new EventEmitter()
 
-  const emit: EmitEvent<SynorMigratorEvent, any> = (event, data) => {
+  const emit: SynorEventEmitter['emit'] = (event, ...data) => {
     eventEmitter.emit(event, data)
   }
 
-  const on: OnEvent<SynorMigratorEvent, any> = (event, listener) => {
+  const on: SynorEventEmitter['on'] = (event, listener) => {
+    // @ts-ignore
     eventEmitter.on(event, listener)
   }
 
