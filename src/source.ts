8deda5b703afbad63c1d04bf37b7d1e2a221b1f3
MunifTanjim <muniftanjim@gmail.com>
2019-12-10 21:31:04 +0600

    Add src/source

---
+++ src/source.ts
@@ -0,0 +1,32 @@
+type MigrationInfo = import('./migration').MigrationInfo
+type MigrationType = import('./migration').MigrationType
+type SynorConfig = import('.').SynorConfig
+
+export interface SourceEngine {
+  open(): Promise<void>
+  close(): Promise<void>
+  first(): Promise<string | null>
+  prev(version: string): Promise<string | null>
+  next(version: string): Promise<string | null>
+  last(): Promise<string | null>
+  get(version: string, type: MigrationType): Promise<MigrationInfo | null>
+  read(migrationInfo: MigrationInfo): Promise<Buffer>
+}
+
+export type SourceEngineFactory = (
+  uri: SynorConfig['sourceUri'],
+  helpers: Pick<SynorConfig, 'migrationInfoParser'>
+) => SourceEngine
+
+type SynorSourceConfig = Pick<
+  SynorConfig,
+  'SourceEngine' | 'sourceUri' | 'migrationInfoParser'
+>
+
+export function SynorSource({
+  SourceEngine,
+  sourceUri,
+  migrationInfoParser
+}: SynorSourceConfig): SourceEngine {
+  return SourceEngine(sourceUri, { migrationInfoParser })
+}
